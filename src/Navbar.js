a1af443a644d458abbaf2deb9058fd3f2b91efdd
varu1303 <varunrana13@gmail.com>
2022-12-19 14:23:13 +0530

    CSS Classes - global scoped

--- src/Navbar.js
+++ src/Navbar.js
@@ -15,7 +15,7 @@ class Navbar extends Component {
     const { cartItems, onResetCart } = this.props;
 
     return (
-      <div style={styles.nav}>
+      <div className="nav">
         {this.state.showCartList && (
           <CartList
             items={cartItems}
@@ -23,45 +23,18 @@ class Navbar extends Component {
             toggleCartList={this.toggleCartList}
           />
         )}
-        <div style={styles.cartIconContainer} onClick={this.toggleCartList}>
+        <div className="cartIconContainer" onClick={this.toggleCartList}>
           <img
-            style={styles.cartIcon}
+            className="cartIcon"
             src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
             alt="cart-icon"
           />
-          <span style={styles.cartCount}>{cartItems.length}</span>
+          <span className="cartCount">{cartItems.length}</span>
         </div>
       </div>
     );
   }
 }
 
-const styles = {
-  cartIcon: {
-    height: 48,
-    marginRight: 20,
-  },
-  nav: {
-    height: 70,
-    background: "#4267b2",
-    display: "flex",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    position: "relative",
-  },
-  cartIconContainer: {
-    position: "relative",
-    cursor: "pointer",
-  },
-  cartCount: {
-    background: "orange",
-    borderRadius: "50%",
-    padding: "4px 8px",
-    position: "absolute",
-    right: 10,
-    top: -5,
-    fontSize: 12,
-  },
-};
 
 export default Navbar;
